8b49e65491432de873185ff84c0ab78d8a0a315e
RentaroSuzuki <82135239+RentaroSuzuki@users.noreply.github.com>
2021-05-08 18:03:49 +0900

    Update Exercise_4_problem_2.py
    I add my code to the lines 50 and 54.

--- Exercise_4_problem_2.py
+++ Exercise_4_problem_2.py
@@ -47,10 +47,11 @@ det temp_classifier(temp_celsius):
 
 # 1. What is the class value for 16.5 degrees Celsius?
 # YOUR CODE HERE
-#
+print(temp_classifier(16.5))
+
 # 2. What is the class value for +2 degrees Celsius?
 # YOUR CODE HERE
-#
+print(temp_classifier(2))
 
 
 # ### Problem 2 tests
